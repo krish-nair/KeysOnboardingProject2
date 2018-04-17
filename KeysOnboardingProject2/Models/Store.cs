ad7527b6198fcd7eeb17630e51b16e5e07e75ee2
krish-nair <krish.nair101@gmail.com>
2018-04-17 13:02:55 +1200

    Edit Store

--- KeysOnboardingProject2/Models/Store.cs
+++ KeysOnboardingProject2/Models/Store.cs
@@ -8,9 +8,12 @@ namespace KeysOnboardingProject2.Models
 {
     public class Store
     {
-        public int Id { get; set; }
-        [Required]
-        public string Name { get; set; }
-        public String Address { get; set; }
+        public virtual int Id { get; set; }
+        [Display(Name ="Store Name")]
+        [Required(ErrorMessage ="Store name is required")]
+        [RegularExpression(@"^[a-zA-Z0-9'' '] + $", ErrorMessage ="Special charecters not allowed")]
+        public virtual string Name { get; set; }
+        [Display(Name ="Store Name")]
+        public virtual String Address { get; set; }
     }
 }
